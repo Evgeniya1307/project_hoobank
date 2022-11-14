227364e1a103cd17c99eef85ba558e7f685dacd5
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-11-14 16:12:57 +0400

    будет видимым на мобильном устройстве ss:hidden ${styles.flexCenter

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -50,7 +50,7 @@ const Hero = () => {
 <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
 {/* gradient end */}
       </div>
-      <div className={`ss:hidden ${styles.flexCenter}`}>
+      <div className={`ss:hidden ${styles.flexCenter}`}> {/*будет видимым на мобильном устройстве */}
       <GetStarted />
     </div>
   </section>
