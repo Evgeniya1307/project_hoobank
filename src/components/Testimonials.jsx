b289e01e298f384a30c5031430c61b861e0a7648
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-11-17 14:40:14 +0400

    map((client) => (//показываю что это за клиенты

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -10,7 +10,8 @@ const Testimonials = () => (
     <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
-      <h2 className={styles.heading2}>
+      
+    <h2 className={styles.heading2}>
         What People are <br className="sm:block hidden" /> saying about us
       </h2>
       <div className="w-full md:mt-0 mt-6">
@@ -22,7 +23,7 @@ const Testimonials = () => (
     </div>
 
     <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
-      {feedback.map((card) => <FeedbackCard key={card.id} {...card} />)}
+      {feedback.map((card) => <FeedbackCard key={card.id} {...card} />)}{/*получаю каждый отдельный элемент обратной связи (карточкй) и вернуть компонент карточки и распространяю свойство карты */}
     </div>
   </section>
 );
