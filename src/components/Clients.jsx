7341eedfef5d24df07ee12b996711d53e83c4dab
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-11-17 14:44:15 +0400

    призыв к дейсвию const CTA

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -4,7 +4,7 @@ import styles from "../style";
 const Clients = () => (
   <section className={`${styles.flexCenter} my-4`}>
     <div className={`${styles.flexCenter} flex-wrap w-full`}>
-      {clients.map((client) => (//показываю что это за клиенты
+      {clients.map((client) => (//получаю 1 отдельного клиента и верну client.id
         <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}>
           <img src={client.logo} alt="client_logo" className="sm:w-[192px] w-[100px] object-contain" />
         </div>
