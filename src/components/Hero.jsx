d07e827fcc9a796ba3016a88c6ab9616541573b4
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2023-03-16 12:02:33 +0400

    id rare home scroll

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
       id="home"
       className={`flex md:flex-row flex-col ${styles.paddingY}`}
     >
-      {/*id равный home чтобы прокручивать  */}
+      {/*id rare home scroll  */}
       <div
         className={`flex-1 ${styles.flexStart}
    flex-col xl:px-0 sm:px-16 px-6`}
